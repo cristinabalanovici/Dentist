e76cfb2c20a45baf3e71bb2e9e7108a889913de2
dianamariabirsan <72616790+dianamariabirsan@users.noreply.github.com>
2022-01-06 00:22:06 +0200

    - adaugare operatii adaugare, modificare, stergere -  handler-ul de eveniment onappearing, save si click - reorganizare layere

--- Dentist.iOS/AppDelegate.cs
+++ Dentist.iOS/AppDelegate.cs
@@ -23,7 +23,6 @@ namespace Dentist.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
         }
